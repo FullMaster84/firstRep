4ad6605c0cb7fb9d34782dc959529552aed9c1cd
Alexandr <89397425+FullMaster84@users.noreply.github.com>
2021-08-27 15:11:11 +0300

    Add files via upload

--- kvadro1.c
+++ kvadro1.c
@@ -2,37 +2,55 @@
 #include <stdlib.h>
 #include <locale.h>
 
-int main()
-{
-setlocale (LC_ALL, "Rus");
-double a, b, c, Discr, x1, x2;
-printf ("Квадратное уравнение имеет вид ax^2+bx+c=0 \n");
-printf ("Введите переменные а, b, c: ");
-scanf ("%lg %lg %lg", &a, &b, &c);
-
-Discr= b*b - 4*a*c;
-if (a==0){
-    if (b==0) {
-        c=0;
-        printf ("Бесконечное количество решений");
+// Проверить верность ввода scanf, несколько попыток
+// функции
+int main (){
+    setlocale (LC_ALL, "Rus");
+    printf ("\n\n ----------------------- \n Квадратное уравнение имеет вид ax^2+bx+c=0 \n");
+    printf (" Введите переменные а, b, c: ");
+    double a, b, c, Discr, x1, x2;
+    int ret = scanf ("%lg %lg %lg", &a, &b, &c);
+    int Quad_eq ()
+    {
+        if (ret == 3){
+            if (a == 0) {
+                if (b == 0) {
+                    return 3;
+                }
+                else {
+                    x1 = -c/b;
+                    return 1;
+                }
+            }
+            else {
+                Discr = b*b - 4*a*c;
+                if (Discr >= 0) {
+                    printf ("Discr = %lg", Discr);
+                    printf ("\n Ret = %d", ret);
+                    x1 = (-b + sqrt(Discr)) / (2*a);
+                    x2 = (-b - sqrt(Discr)) / (2*a);
+                    return 2;
+                    }
+                else {
+                    return 0;
+                    }
         }
-    else {
-        x1= -c/b;
-        printf ("\n x1= %lg \n", x1);
-        }
-}
-else{
-    if (Discr>=0) {
-        x1= (-b+sqrt(Discr))/(2*a);
-        printf ("\n x1= %lg \n", x1);
-        x2= (-b-sqrt(Discr))/(2*a);
-        printf ("x2= %lg \n", x2);
-        }
-    else {
-        printf ("\n Нет решения в вещественных числах");
-        }
-
-}
-
-return 0;
-}
+    }
+         else {
+            return -1;
+         }
+    }
+        int nRoots = Quad_eq(a, b, c, &x1, &x2);
+        switch (nRoots){
+            case 3: printf ("Бесконечное количество решений");
+            break;
+            case 1: printf (" x1 = %lg", x1);
+            break;
+            case 2: printf ("\n x1= %lg \n  x2= %lg \n", x1, x2);
+            break;
+            case 0: printf ("\n Нет решений в вещественных числах");
+            break;
+            case -1: printf("\n Вы ввели ахинею \n\n Попробуйте снова!!!\n\n"); return main ();
+            break;
+            }
+    }
