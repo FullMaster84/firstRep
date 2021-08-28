d3b2c5fa08a7c64b9f2f32b0e39b97a2f6b4deb3
Alexandr <89397425+FullMaster84@users.noreply.github.com>
2021-08-29 00:00:22 +0300

    Update kvadro1.c

--- kvadro1.c
+++ kvadro1.c
@@ -1,54 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <math.h>
 
-// Ïðîâåðèòü âåðíîñòü ââîäà scanf, íåñêîëüêî ïîïûòîê
-// ôóíêöèè
-int main (){
-    setlocale (LC_ALL, "Rus");
-    printf ("\n\n ----------------------- \n Êâàäðàòíîå óðàâíåíèå èìååò âèä ax^2+bx+c=0 \n");
-    printf (" Ââåäèòå ïåðåìåííûå à, b, c: ");
-    double a, b, c, Discr, x1, x2;
-    int ret = scanf ("%lg %lg %lg", &a, &b, &c);
-    int Quad_eq ()
-    {
-        if (ret == 3){
-            if (a == 0) {
-                if (b == 0) {
-                    return 3;
-                }
-                else {
-                    x1 = -c/b;
-                    return 1;
-                }
-            }
-            else {
-                Discr = b*b - 4*a*c;
-                if (Discr >= 0) {
-                    x1 = (-b + sqrt(Discr)) / (2*a);
-                    x2 = (-b - sqrt(Discr)) / (2*a);
-                    return 2;
-                    }
-                else {
-                    return 0;
-                    }
+//Рассказать  про то, что нужно вынести в отдельные файлы, раздельная линковка, хэдеры
+
+enum NUMROOTS {
+    NO_ROOTS = 0,
+    ONE_ROOT = 1,
+    TWO_ROOTS = 2,
+    INF_ROOTS = 3,
+};
+
+int LineEq (const double b, const double c, double* x1) {
+    if (b == 0) {
+        if (c == 0) {
+            return INF_ROOTS;
+        }
+        else {
+            return NO_ROOTS;
         }
     }
-         else {
-            return -1;
-         }
+    else {
+        *x1 = -c / b;
+        return ONE_ROOT;
     }
-        int nRoots = Quad_eq(a, b, c, &x1, &x2);
-        switch (nRoots){
-            case 3: printf ("Áåñêîíå÷íîå êîëè÷åñòâî ðåøåíèé");
-            break;
-            case 1: printf (" x1 = %lg", x1);
-            break;
-            case 2: printf ("\n x1= %lg \n  x2= %lg \n", x1, x2);
-            break;
-            case 0: printf ("\n Íåò ðåøåíèé â âåùåñòâåííûõ ÷èñëàõ");
-            break;
-            case -1: printf("\n Âû ââåëè àõèíåþ \n\n Ïîïðîáóéòå ñíîâà!!!\n\n"); return main ();
-            break;
-            }
+}
+
+
+int QuadEq (const double a, const double b, const double c, double* x1, double* x2) {
+    double discr = b * b - 4 * a * c;
+
+    if (discr >= 0) {
+        *x1 = (-b + sqrt(discr)) / (2 * a);
+        *x2 = (-b - sqrt(discr)) / (2 * a);
+        return TWO_ROOTS;
     }
+    else {
+        return NO_ROOTS;
+    }
+
+}
+
+
+int combo_eq (const double a, const double b, const double c, double* x1, double* x2) { //TODO Name of fucntion
+    if (a == 0) {
+        return LineEq (b, c, x1);
+    }
+    else {
+        return QuadEq (a, b, c, x1, x2);
+    }
+}
+
+void NumVariables () {
+    printf("\n Вы ввели ахинею \n\n Попробуйте снова!!!\n\n");
+    while (getchar() != '\n') { continue; }
+}
+
+int main () {
+    setlocale (LC_ALL, "Rus");
+    printf ("\n\n ----------------------- \n Квадратное уравнение имеет вид ax^2+bx+c=0 \n");
+    printf (" Введите переменные а, b, c: ");
+    double a = 0, b = 0, c = 0;
+    double x1 = 0, x2 = 0;
+    while (scanf ("%lg %lg %lg", &a, &b, &c) != 3)  {
+        NumVariables ();
+    }
+    int nRoots = combo_eq(a, b, c, &x1, &x2);
+    switch (nRoots) {
+        case INF_ROOTS: printf ("\n Бесконечное количество решений \n");
+        break;
+
+        case ONE_ROOT: printf (" x1 = %lg", x1);
+        break;
+
+        case TWO_ROOTS: printf ("\n x1= %lg \n x2= %lg \n", x1, x2);
+        break;
+
+        case NO_ROOTS: printf ("\n Нет решений в вещественных числах \n");
+        break;
+     }
+
+    return 0;
+}
